6b2147d2658fea1fa1fa0e3e5825cb3150953d4a
tobi08151405 <tobias.teichmann@gmx.at>
2019-10-13 13:22:19 +0200

    added support for encoders and restructured parts of main.py

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import socket
 
 serial_enable = False
 
-rows = 2
-cols = 2
-faders = 2
+rows = 4
+cols = 3
+faders = 3
+encoders = 1
 universe_num = 4
 uni_map = dict(zip(range(universe_num), range(universe_num)))
 uni_map_ = {v: k for k, v in uni_map.items()}
@@ -63,6 +64,7 @@ class ArtnetThread(QThread):
 class SerialThread(QThread):
     keystroke = pyqtSignal(str, bool)
     fadermove = pyqtSignal(str, int)
+    encodermove = pyqtSignal(str, int)
     
     def __init__(self):
         QThread.__init__(self)
@@ -84,6 +86,9 @@ class SerialThread(QThread):
                 if "A" in serial_get:
                     spacer = serial_get.index(":")
                     self.fadermove.emit(serial_get[3:spacer], int(serial_get[spacer+1:-5]))
+                elif "E" in serial_get:
+                    spacer = serial_get.index(":")
+                    self.encodermove.emit(serial_get[3:spacer], int(serial_get[spacer+1:-5]))
                 else:
                     self.keystroke.emit(serial_get[2:-6], bool(int(serial_get[-6])))
             except:
@@ -100,25 +105,12 @@ class MainWindow(QMainWindow):
             serial_thread = SerialThread()
             serial_thread.keystroke.connect(self.map_keys)
             serial_thread.fadermove.connect(self.map_faders)
+            serial_thread.encodermove.connect(self.map_encoders)
             serial_thread.start()
         
         artnet_thread = ArtnetThread()
         self.channelset.connect(artnet_thread.set_channel)
         artnet_thread.start()
-        
-        self.layout = QGridLayout()
-        
-        for row in range(rows):
-            for col in range(cols):
-                num = (row * cols) + col
-                exec("self.key"+str(num)+"=QPushButton()")
-                exec("self.key"+str(num)+".setCheckable(True)")
-                exec("self.layout.addWidget(self.key"+str(num)+","+str(row)+","+str(col)+")")
-        
-        for fader in range(faders):
-            exec("self.fader"+str(fader)+"=QLabel()")
-            exec("self.fader"+str(fader)+".setAlignment(Qt.AlignCenter)")
-            exec("self.layout.addWidget(self.fader"+str(fader)+","+str(rows+1)+","+str(fader)+")")
         
         sortact = QAction('Sort', self)
         sortact.triggered.connect(self.sort)
@@ -131,33 +123,86 @@ class MainWindow(QMainWindow):
         self.fileMenu.addAction(sortact)
         self.fileMenu.addAction(quitact)
         
-        self.widget = QWidget()
-        self.widget.setLayout(self.layout)
-        
         self.mdi = QMdiArea()
         
-        self.sub = QMdiSubWindow()
-        self.sub.setWidget(self.widget)
-        self.mdi.addSubWindow(self.sub)
-        self.sub.show()
+        self.create_encoders()
+        self.create_faders()
+        self.create_keys()
         
         self.setCentralWidget(self.mdi)
+    
+    def create_keys(self):
+        self.keys_layout = QGridLayout()
+        for row in range(rows):
+            for col in range(cols):
+                num = (row * cols) + col
+                exec("self.key{0:d}=QPushButton()".format(num))
+                exec("self.key{0:d}.setCheckable(True)".format(num))
+                exec("self.keys_layout.addWidget(self.key{0:d},{1:d},{2:d})".format(num,row,col))
+        self.keys_widget = QWidget()
+        self.keys_widget.setLayout(self.keys_layout)
+        self.keys_sub = QMdiSubWindow()
+        self.keys_sub.setWidget(self.keys_widget)
+        self.mdi.addSubWindow(self.keys_sub)
+        self.keys_sub.show()
+    
+    def create_faders(self):
+        self.faders_layout = QGridLayout()
+        for fader in range(faders):
+            exec("self.fader{0:d}=QLabel()".format(fader))
+            exec("self.fader{0:d}.setAlignment(Qt.AlignCenter)".format(fader))
+            exec("self.fader{0:d}.setText('0')".format(fader))
+            exec("self.faders_layout.addWidget(self.fader{0:d},0,{0:d})".format(fader))
+        self.faders_widget = QWidget()
+        self.faders_widget.setLayout(self.faders_layout)
+        self.faders_sub = QMdiSubWindow()
+        self.faders_sub.setWidget(self.faders_widget)
+        self.mdi.addSubWindow(self.faders_sub)
+        self.faders_sub.show()
+        
+    def create_encoders(self):
+        self.encoders_layout = QGridLayout()
+        for encoder in range(encoders):
+            exec("self.encoder{0:d}=QLabel()".format(encoder))
+            exec("self.encoder{0:d}.setAlignment(Qt.AlignCenter)".format(encoder))
+            exec("self.encoder{0:d}.setText('0')".format(encoder))
+            exec("self.encoders_layout.addWidget(self.encoder{0:d},0,{0:d})".format(encoder))
+        self.encoders_widget = QWidget()
+        self.encoders_widget.setLayout(self.encoders_layout)
+        self.encoders_sub = QMdiSubWindow()
+        self.encoders_sub.setWidget(self.encoders_widget)
+        self.mdi.addSubWindow(self.encoders_sub)
+        self.encoders_sub.show()
     
     def sort(self):
         self.mdi.tileSubWindows()
     
     @pyqtSlot(str, bool)
     def map_keys(self, key, pressed):
-        if pressed:
-            exec("self.key"+key+".setChecked(True)")
-            exec("self.key"+key+".setText('pressed')")
-        else:
-            exec("self.key"+key+".setChecked(False)")
-            exec("self.key"+key+".setText('')")
+        try:
+            if pressed:
+                exec("self.key{0:s}.setChecked(True)".format(key))
+                exec("self.key{0:s}.setText('pressed')".format(key))
+            else:
+                exec("self.key{0:s}.setChecked(False)".format(key))
+                exec("self.key{0:s}.setText('')".format(key))
+        except NameError:
+            print("button {0:s} not found!".format(key))
             
     @pyqtSlot(str, int)
     def map_faders(self, fader, value):
-        exec("self.fader"+fader+".setText('"+str(value)+"')")
+        try:
+            exec("self.fader{0:s}.setText('{1:d}')".format(fader,value))
+        except NameError:
+            print("fader {0:s} not found!".format(fader))
+    
+    @pyqtSlot(str, int)
+    def map_encoders(self, encoder, value):
+        try:
+            exec("cur = int(self.encoder{0:s}.text())".format(encoder))
+            exec("self.encoder{0:s}.setText('{1:d}')".format(encoder, cur-value))
+        except NameError:
+            print("encoder {0:s} not found!".format(encoder))
 
 app = QApplication(sys.argv)
 
